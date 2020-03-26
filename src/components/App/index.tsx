4d29b62408e1c7edf6a3348231fb0274ae8cf6c6
Stefan Bajić <stefanbajic@posteo.net>
2020-03-26 15:05:28 +0100

    initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./App.scss"
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="Header">Header</div>
+      <div className="Body">Body</div>
+    </div>
+  )
+}
+
+export default App;
